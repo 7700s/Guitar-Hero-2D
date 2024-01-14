0d90c32d2c71836ef059b2e716509a925932d51d
Frogluba <138372129+7700s@users.noreply.github.com>
2024-01-14 11:43:07 +0200

    Update main.cs

--- Assets/main.cs
+++ Assets/main.cs
@@ -6,7 +6,7 @@ public class NewBehaviourScript : MonoBehaviour
 {
     // Start is called before the first frame update
     public Transform FCircle1, FCircle2, FCircle3, FCircle4;
-    private Vector3 startPos;
+    private float startPos;
     private float endPos;
     private float speed = 2.0f;
     private float minYtop;
@@ -18,7 +18,7 @@ public class NewBehaviourScript : MonoBehaviour
     void Start()
     {
         speed *= -1f;
-        startPos = FCircle1.position;
+        startPos = FCircle1.position.y;
         endPos = -5.29f;
         minYtop =  -3.9f;
         minYbot = -4.61f;
@@ -30,13 +30,14 @@ public class NewBehaviourScript : MonoBehaviour
     void MoveCircles()
     {
         FCircle1.position += new Vector3(0, speed * Time.deltaTime);
-        if (FCircle1.position.y < endPos) FCircle1.position = startPos;
+        if (FCircle1.position.y < endPos) FCircle1.position = new Vector3(FCircle1.position.x,startPos);
         
     }
 
     void Click()
     {
-        if (Input.GetKey(KeyCode.A))
+        #region f1
+        if (Input.GetKey(KeyCode.H))
         {
             if(FCircle1.position.y < minYtop && FCircle1.position.y > minYbot)
             {
@@ -48,8 +49,28 @@ public class NewBehaviourScript : MonoBehaviour
                 {
                     print("1 good");
                 }
+                FCircle1.position = FCircle1.position = new Vector3(FCircle1.position.x, startPos);
             }
         }
+        #endregion f1
+        #region f2
+        if (Input.GetKey(KeyCode.J))
+        {
+            if (FCircle2.position.y < minYtop && FCircle1.position.y > minYbot)
+            {
+                if (FCircle2.position.y < perfectYtop && FCircle2.position.y > perfectYbot)
+                {
+                    print("2 perfect");
+                }
+                else
+                {
+                    print("2 good");
+                }
+                FCircle2.position = FCircle2.position = new Vector3(FCircle2.position.x, startPos);
+            }
+        }
+        #endregion f2
+
     }
 
     // Update is called once per frame
